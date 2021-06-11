e3c02909bf9109a8ec1f004fef50ea3c8643367e
waklele <complexrangga@gmail.com>
2021-06-11 15:33:19 +0700

    Initial Commit

---
+++ JournalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JournalApp
+//
+//  Created by Rangga Rentya on 11/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
