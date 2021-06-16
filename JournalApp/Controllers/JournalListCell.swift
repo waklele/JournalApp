9bf5ce9c249c92cf3474e7aefaef068a13623092
Rizki Miftha <riskimifta@gmail.com>
2021-06-16 16:12:08 +0700

    Edit2 file

---
+++ JournalApp/Controllers/JournalListCell.swift
@@ -0,0 +1,17 @@
+//
+//  JournalListCell.swift
+//  JournalApp
+//
+//  Created by Muhammad Rizki Miftha Alhamid on 6/16/21.
+//
+
+import UIKit
+
+class JournalListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
