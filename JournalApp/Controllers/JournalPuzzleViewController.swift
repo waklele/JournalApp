e687b5033e7a92536a3696f9df322571ea0cc617
Albert <albertonly898@gmail.com>
2021-06-21 17:35:44 +0700

    Puzzle page and add asset

--- JournalApp/Controllers/JournalPuzzleViewController.swift
+++ JournalApp/Controllers/JournalPuzzleViewController.swift
@@ -8,26 +8,47 @@
 import UIKit
 
 class JournalPuzzleViewController: UIViewController {
-
+    @IBOutlet weak var puzzle: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        puzzle.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapAction))
+        self.puzzle.addGestureRecognizer(tapGestureRecognizer)
     }
     
+    @objc func tapAction(sender: UITapGestureRecognizer) {
 
+         let touchPoint = sender.location(in: self.puzzle)
+         print(touchPoint)
+
+        let z1 = touchPoint.x
+        let z2 = touchPoint.y
+         print("Before Alert Touched point (\(z1), \(z2)")
+
+         //convert point into Percentage
+         let z1per =  z1 * 100 / self.puzzle.frame.size.width
+         let z2per =  z2 * 100 / self.puzzle.frame.size.height
+
+         print("After Alert Touched point (\(z1per), \(z2per)")
+        
+        
+   }
+    
+    
     @IBAction func saveJournal(_ sender: Any) {
         navigationController?.popToRootViewController(animated: true)
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
