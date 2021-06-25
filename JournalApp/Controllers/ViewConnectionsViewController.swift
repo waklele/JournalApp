f430792985919da69a708a8ce5d001fed1131353
Rizki Miftha <riskimifta@gmail.com>
2021-06-25 12:00:04 +0700

    udah bisa making connections, tpi blum save ke core data

---
+++ JournalApp/Controllers/ViewConnectionsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewConnectionsViewController.swift
+//  JournalApp
+//
+//  Created by Muhammad Rizki Miftha Alhamid on 6/25/21.
+//
+
+import UIKit
+import CoreData
+
+class ViewConnectionsViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var promptLabel: UILabel!
+    @IBOutlet weak var detailsTextView: UITextView!
+    
+    public var puzzleType: Int = 0
+    public var dataId: Int64 = 0
+    public var readingTitle = String()
+    public var puzzle1Detail = String()
+    public var puzzle2Detail = String()
+    public var puzzle3Detail = String()
+    public var puzzle4Detail = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        self.title = ""
+
+        detailsTextView.layer.cornerRadius = 5
+        detailsTextView.delegate = self
+        detailsTextView.text = "Insert detail here..."
+        detailsTextView.textColor = UIColor.lightGray
+        
+        let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: nil, action: #selector(editConnections))
+        navigationItem.rightBarButtonItem = editButton
+        
+        adjustPrompt()
+    }
+    
+    func adjustPrompt() {
+        if puzzleType == 1 {
+            promptLabel.text = "Bagaimana cerita ini serupa/berbeda dengan kehidupan kamu?"
+            self.title = "Text-to-Self"
+        } else if puzzleType == 2 {
+            promptLabel.text = "Bagaimana cerita ini mengingatkan kamu dengan cerita lain?"
+            self.title = "Text-to-Text"
+        } else if puzzleType == 3 {
+            promptLabel.text = "Bagaimana cerita ini mengingatkan kamu pada dunia disekitarmu?"
+            self.title = "Text-to-World"
+        } else {
+            print("error")
+        }
+    }
+    
+    @objc func editConnections() {
+        
+    }
+}
